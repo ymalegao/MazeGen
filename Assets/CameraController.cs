8e9b469fb71e421f4c15de0d7adf8a6f188369d5
jkawai35 <jkawai@ucsc.edu>
2024-05-31 17:18:39 -0700

    camera movement on test scene

---
+++ Assets/CameraController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CameraController : MonoBehaviour
+{
+    private float x;
+    private float y;
+    public float sensitivity = -1f;
+    private Vector3 rotate;
+    Vector3 moveDirection = Vector3.zero;
+    public float moveSpeed = 6f;
+
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = moveSpeed * Input.GetAxis("Vertical");
+        float curSpeedY = moveSpeed * Input.GetAxis("Horizontal");
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        y = Input.GetAxis("Mouse X");
+        x = Input.GetAxis("Mouse Y");
+        rotate = new Vector3(x, y * sensitivity, 0);
+        transform.eulerAngles = transform.eulerAngles - rotate;
+
+
+    }
+}
